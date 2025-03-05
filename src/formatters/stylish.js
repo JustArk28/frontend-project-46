0384f84aba7103a86cca270b8ed1c81483522761
JustArk28 <arkadiyarkadiy95@gmail.com>
2025-03-05 22:16:50 +0700

    add stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,14 +1,14 @@
 import _ from 'lodash';
 
 const tab = ' ';
-const tabSize = 4;
+const tabSize2 = 4;
 const offset = 2;
 
 const getPrefix = (level, tabSize, offset) => tab.repeat(level * tabSize - offset);
 
 const checkOfValue = (data, deep) => {
-  const formattedValue = _.isPlainObject(data) 
-  ? '{' + Object.entries(data).map(([key, value]) => `\n${getPrefix(deep, tabSize, offset - 6)}${key}: ${checkOfValue(value, deep + 1)}`).join('') + `\n${getPrefix(deep, tabSize, offset - 2)}}`
+  const formattedValue = _.isPlainObject(data)
+  ? '{' + Object.entries(data).map(([key, value]) => `\n${getPrefix(deep, tabSize2, offset - 6)}${key}: ${checkOfValue(value, deep + 1)}`).join('') + `\n${getPrefix(deep, tabSize2, offset - 2)}}`
     : data;
   return formattedValue;
 };
@@ -18,20 +18,20 @@ const stylish = (array) => {
     const result = array.map((obj) => {
       switch (obj.type) {
         case 'added':
-          return `\n${getPrefix(level, tabSize, offset)}+ ${obj.key}: ${checkOfValue(obj.value, level)}`;
+          return `\n${getPrefix(level, tabSize2, offset)}+ ${obj.key}: ${checkOfValue(obj.value, level)}`;
         case 'deleted':
-          return `\n${getPrefix(level, tabSize, offset)}- ${obj.key}: ${checkOfValue(obj.value, level)}`;
-        case 'changed': 
-          return `\n${getPrefix(level, tabSize, offset)}- ${obj.key}: ${checkOfValue(obj.value1, level)}\n${getPrefix(level, tabSize, offset)}+ ${obj.key}: ${checkOfValue(obj.value2, level)}`;
+          return `\n${getPrefix(level, tabSize2, offset)}- ${obj.key}: ${checkOfValue(obj.value, level)}`;
+        case 'changed':
+          return `\n${getPrefix(level, tabSize2, offset)}- ${obj.key}: ${checkOfValue(obj.value1, level)}\n${getPrefix(level, tabSize2, offset)}+ ${obj.key}: ${checkOfValue(obj.value2, level)}`;
         case 'nested':
-          return `\n${getPrefix(level, tabSize, offset)}  ${obj.key}: ${depth(obj.value, level + 1)}`;
+          return `\n${getPrefix(level, tabSize2, offset)}  ${obj.key}: ${depth(obj.value, level + 1)}`;
         case 'unchanged':
-          return `\n${getPrefix(level, tabSize, offset)}  ${obj.key}: ${checkOfValue(obj.value, level)}`;
+          return `\n${getPrefix(level, tabSize2, offset)}  ${obj.key}: ${checkOfValue(obj.value, level)}`;
         default:
           throw new Error('Something wrong');
       }
     }).join('');
-    return `{${result}\n${getPrefix(level, tabSize, offset + 2)}}`;
+    return `{${result}\n${getPrefix(level, tabSize2, offset + 2)}}`;
   };
   return depth(array, 1);
 };
