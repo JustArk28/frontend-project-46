800f0921cbe4a6ea26c55b1d0f543cdc21fb6c24
JustArk28 <arkadiyarkadiy95@gmail.com>
2025-03-23 15:36:52 +0700

    fixed formatters/index.js #2

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -4,24 +4,11 @@ import json from './json.js';
 
 const format = (data, format1) => {
   const formats = {
-    stylish: stylish,
-    plain: plain,
-    json: json,
+    stylish,
+    plain,
+    json,
   };
   return formats[format1](data);
 };
-export default format;
 
-// const format = (result, format) => {
-//   switch (format) {
-//     case 'stylish':
-//       return stylish(result);
-//     case 'plain':
-//       return plain(result);
-//     case 'json':
-//       return json(result);
-//     default:
-//       throw Error(`incorrect format "${format}"`);
-//   }
-// };
-// export default format;
+export default format;
