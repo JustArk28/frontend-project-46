d2f64e8b03cf502e5d45319de99c71df4893e5b5
JustArk28 <arkadiyarkadiy95@gmail.com>
2025-03-23 15:29:07 +0700

    fixed formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,27 +2,26 @@ import plain from './plain.js';
 import stylish from './stylish.js';
 import json from './json.js';
 
-// const format = (data, format) => {
-//   const formats = {
-//     stylish: stylish,
-//     plain: plain,
-//     json: json,
-//   };
-//   return formats[format](data);
+const format = (data, format1) => {
+  const formats = {
+    stylish: stylish,
+    plain: plain,
+    json: json,
+  };
+  return formats[format1](data);
+};
+export default format;
+
+// const format = (result, format) => {
+//   switch (format) {
+//     case 'stylish':
+//       return stylish(result);
+//     case 'plain':
+//       return plain(result);
+//     case 'json':
+//       return json(result);
+//     default:
+//       throw Error(`incorrect format "${format}"`);
+//   }
 // };
 // export default format;
-
-const format = (result, format) => {
-  switch (format) {
-    case 'stylish':
-      return stylish(result);
-    case 'plain':
-      return plain(result);
-    case 'json':
-      return json(result);
-    default:
-      throw Error(`incorrect format "${format}"`);
-  }
-};
-
-export default format;
