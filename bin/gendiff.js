4ba22e64206782061aa1d08b7213aa145d492e44
JustArk28 <arkadiyarkadiy95@gmail.com>
2025-03-23 16:22:06 +0700

    fixed gendiff.js #2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,32 +1,5 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
-import gendiff from '../index.js';
+import program from '../src/program.js';
 
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .arguments('<filepath1> <filepath2>')
-  .version('0.8.0')
-  .option('-f, --format [type]', 'Output format', 'stylish')
-  .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2, program.opts().format));
-  })
-  .parse();
-
-export default program;
-//import program from '../src/program.js';
-// import commander from 'commander';
-// import gendiff from '../index.js';
-
-// commander
-//   .version('0.0.1')
-//   .description('Compares two configuration files and shows a difference.')
-//   .option('-f, --format [type]', 'output format', 'stylish')
-//   .arguments('<filepath1> <filepath2>')
-//   .action((filepath1, filepath2) => {
-//     const diff = gendiff(filepath1, filepath2, commander.format);
-//     console.log(diff);
-//   });
-
-// commander.parse(process.argv);
+program();
--- src/program.js
+++ src/program.js
@@ -1,15 +1,13 @@
-// import { program } from 'commander';
-// import gendiff from '../index.js';
+import { program } from 'commander';
+import gendiff from '../index.js';
 
-// program
-//   .name('gendiff')
-//   .description('Compares two configuration files and shows a difference.')
-//   .arguments('<filepath1> <filepath2>')
-//   .version('0.8.0')
-//   .option('-f, --format [type]', 'Output format', 'stylish')
-//   .action((filepath1, filepath2) => {
-//     console.log(gendiff(filepath1, filepath2, program.opts().format));
-//   })
-//   .parse();
-
-// export default program;
+export default () => program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .version('0.8.0')
+  .option('-f, --format [type]', 'Output format', 'stylish')
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2, program.opts().format));
+  })
+  .parse();
